7ec62c0091b806adf81ab78f41b9fde9d506b3db
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-10-27 11:00:25 +0100

    more accurate description

--- Automaton/Features/DateWithDestiny.cs
+++ Automaton/Features/DateWithDestiny.cs
@@ -49,7 +49,7 @@ public class DateWithDestinyConfiguration
 internal class DateWithDestiny : Tweak<DateWithDestinyConfiguration>
 {
     public override string Name => "Date with Destiny";
-    public override string Description => $"It's a FATE bot. Requires whatever you want for combat. Open the menu with /vfate.";
+    public override string Description => $"Fate tracker and mover. Doesn't handle combat. Open the menu with /vfate.";
 
     public bool active = false;
     private static Vector3 TargetPos;
