41c6bed90ce6821157a8f918d97a6ce4ecf66201
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-11-22 20:59:50 +0100

    add snipes/ipc for liza

---
+++ Automaton/Features/AutoSnipeQuests.cs
@@ -0,0 +1,11 @@
+﻿namespace Automaton.Features;
+
+[Tweak]
+internal class AutoSnipeQuests : Tweak
+{
+    public override string Name => "Sniper no sniping";
+    public override string Description => "Automatically completes snipe quests.";
+
+    public override void Enable() => P.Memory.SnipeHook.Enable();
+    public override void Disable() => P.Memory.SnipeHook.Disable();
+}
---
+++ Automaton/IPC/Provider.cs
@@ -0,0 +1,19 @@
+﻿using ECommons.EzIpcManager;
+
+namespace Automaton.IPC;
+internal class Provider
+{
+    public Provider() => EzIPC.Init(this);
+
+    [EzIPC]
+    public bool IsTweakEnabled(string assemblyName) => C.EnabledTweaks.Contains(assemblyName);
+
+    [EzIPC]
+    public void SetTweakState(string assemblyName, bool state)
+    {
+        if (state)
+            C.EnabledTweaks.Add(assemblyName);
+        else
+            C.EnabledTweaks.Remove(assemblyName);
+    }
+}
